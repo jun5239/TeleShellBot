1146597fb6ec8c65d9a22d1d75ab3e7b8eeac608
jun5239 <jun5239@outlook.com>
2024-12-16 14:25:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     config:function(){
         return (
             {
-                adminUsers:[ADMIN_ID], //admin users' telegram id, should be numbers
-                botToken: 'YOUR_BOT_TOEKN', // bot token
+                adminUsers:[1878794912], //admin users' telegram id, should be numbers
+                botToken: '7544006967:AAHzhJo9OqVJw3HG_nBGvMctWAf97jD5z_Y', // bot token
 
             }
         );
